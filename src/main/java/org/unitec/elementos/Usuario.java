5c34dbfa66f12de4ff47aba2f50458465ee6cf47
CarlosRdeV <CarlosRdeV@10.30.10.182>
2019-02-19 20:04:39 -0600

    CrossOrigin2

---
+++ src/main/java/org/unitec/elementos/Usuario.java
@@ -0,0 +1,36 @@
+package org.unitec.elementos;
+
+public class Usuario {
+
+    Integer id;
+    String nombre;
+    Localizacion localizacion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Localizacion getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(Localizacion localizacion) {
+        this.localizacion = localizacion;
+    }
+
+    public Usuario() {
+    }
+
+}
