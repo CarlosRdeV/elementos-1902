95388a9825a1166d2af289fbaf9ed02715932e4f
CarlosRdeV <CarlosRdeV@10.30.10.182>
2019-02-22 21:41:15 -0600

    Controlador Usuario y CRUD

--- src/main/java/org/unitec/elementos/ControladorUsuario.java
+++ src/main/java/org/unitec/elementos/ControladorUsuario.java
@@ -1,6 +1,10 @@
 package org.unitec.elementos;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin //Origenes cruzamos, Heroku hospeda el cerebro, Mlab la persistencia (base de datos),todo en diferentes servidores con cloters elasticos 
 //Necesitamos el CrossOrigin para realizar aplicaciones distribuidas!
 public class ControladorUsuario {
+    @Autowired RepoUsuario repoU;
+    //1. guardar :save()
+    @PostMapping("/usuario")
+    Estatus guardar(@RequestBody String json) throws Exception{
     
+        ObjectMapper maper=new ObjectMapper();
+        Usuario u=maper.readValue(json, Usuario.class);
+        repoU.save(u);
+        Estatus e=new Estatus("Usuario Guardado", true);
+        return e;
+    }
 }
