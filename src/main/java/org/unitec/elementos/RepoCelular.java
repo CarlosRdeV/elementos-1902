b4297535d9caff67e9360485a2c4049d8ab7d214
CarlosRdeV <CarlosRdeV@10.30.10.182>
2019-07-09 20:17:42 -0500

    Clase celular

---
+++ src/main/java/org/unitec/elementos/RepoCelular.java
@@ -0,0 +1,13 @@
+
+package org.unitec.elementos;
+
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author CarlosRdeV
+ */
+public interface RepoCelular extends MongoRepository<Celular, Long>{
+     List<Usuario> findByNombre(String nombre);
+}
