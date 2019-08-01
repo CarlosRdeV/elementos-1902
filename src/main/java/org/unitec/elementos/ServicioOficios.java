3d6e2e9e93791546fa2181f92192389b3ff9d7ee
CarlosRdeV <CarlosRdeV@10.30.10.182>
2019-08-01 00:42:22 -0500

    Se arreglo la clase de serviciooficios, agregaron constructores, tostring y getters and setters

--- src/main/java/org/unitec/elementos/ServicioOficios.java
+++ src/main/java/org/unitec/elementos/ServicioOficios.java
@@ -13,4 +13,80 @@ public class ServicioOficios {
     private String hora;
     private String estatus;
     //En espera, En Proceso, Terminado, Cancelado
+
+    public ServicioOficios(String id, String emailEmpleador, String emailTrabajador, String descripcion, String fecha, String hora, String estatus) {
+        this.id = id;
+        this.emailEmpleador = emailEmpleador;
+        this.emailTrabajador = emailTrabajador;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.estatus = estatus;
+    }
+
+    public ServicioOficios() {
+    }
+
+    @Override
+    public String toString() {
+        return "ServicioOficios{" + "id=" + id + ", emailEmpleador=" + emailEmpleador + ", emailTrabajador=" + emailTrabajador + ", descripcion=" + descripcion + ", fecha=" + fecha + ", hora=" + hora + ", estatus=" + estatus + '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmailEmpleador() {
+        return emailEmpleador;
+    }
+
+    public void setEmailEmpleador(String emailEmpleador) {
+        this.emailEmpleador = emailEmpleador;
+    }
+
+    public String getEmailTrabajador() {
+        return emailTrabajador;
+    }
+
+    public void setEmailTrabajador(String emailTrabajador) {
+        this.emailTrabajador = emailTrabajador;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+    
+    
 }
